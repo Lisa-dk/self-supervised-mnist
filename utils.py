b8a9ab37419a01d0674339fcd6909cd785f28544
Lisa-dk <lisadkoopmans@gmail.com>
2024-04-26 14:10:05 +0200

    Added plots and augmentation changes

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ def generate_and_save_images(model, epoch, test_input):
 
     for i in range(predictions.shape[0]):
         plt.subplot(4, 6, i+1)
-        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.imshow(predictions[i, :, :, 0], cmap='gray')
         plt.axis('off')
 
     plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
